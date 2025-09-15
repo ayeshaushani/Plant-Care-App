d14451d297b698a69bbc1f44d7b48aacee6e9311
Ayesha Ushani <akisasenari@gmail.com>
2025-09-15 15:04:01 +0530

    plant/_layouut.tsx implement

--- app/(dashboard)/plants/_layout.tsx
+++ app/(dashboard)/plants/_layout.tsx
@@ -1,11 +1,23 @@
+// app/(dashboard)/plants/_layout.tsx
 import { Stack } from "expo-router";
 
 export default function PlantLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "My Plants" }} />
-      <Stack.Screen name="add" options={{ title: "Add Plant" }} />
-      <Stack.Screen name="[id]" options={{ title: "Plant Details" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "My Plants",
+          headerTitleAlign: "center",
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
+        options={{
+          title: "Plant Details",
+          headerBackTitle: "Back",
+        }}
+      />
     </Stack>
   );
 }
